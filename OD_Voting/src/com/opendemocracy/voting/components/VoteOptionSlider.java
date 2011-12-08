dd8a8399a1cc8c7d53a550ca0443d28b5199285b
jgullstr <jgullstr@abo.fi>
2011-12-09 00:47:52 +0200

    Missing files

---
+++ OD_Voting/src/com/opendemocracy/voting/components/VoteOptionSlider.java
@@ -0,0 +1,24 @@
+package com.opendemocracy.voting.components;
+
+import com.vaadin.ui.Slider;
+
+public class VoteOptionSlider extends Slider{
+	//TODO: Colors, setMaxAllowed (Disable part > maxallowed) 
+	
+	public VoteOptionSlider(){
+		//super("<b"+caption+"</b>");
+		super();
+		super.setMax(100);
+		super.setMin(-100);
+		super.setWidth("100%");
+		super.addStyleName("option_slider");
+        super.setImmediate(true);
+        
+        //TODO Paint slider red on negative side, green on positive, grey right of maxAllowed
+	}
+
+	//TODO Lock slider to max value max
+	public void setMaxAllowed(double max) {
+
+	}
+}
