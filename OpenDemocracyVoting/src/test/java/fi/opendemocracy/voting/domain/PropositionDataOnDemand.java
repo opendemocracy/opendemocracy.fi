6d4be65ed4509175f9a84b2770c3dd44b21a1bb1
jgullstr <jgullstr@abo.fi>
2012-01-10 21:37:17 +0200

    Clean test

--- OpenDemocracyVoting/src/test/java/fi/opendemocracy/voting/domain/PropositionDataOnDemand.java
+++
@@ -1,7 +0,0 @@
-package fi.opendemocracy.voting.domain;
-
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Proposition.class)
-public class PropositionDataOnDemand {
-}
