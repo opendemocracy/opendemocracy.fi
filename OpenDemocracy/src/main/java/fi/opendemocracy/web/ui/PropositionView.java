891be14cdab175995eb32728a02af146d353b8c5
jgullstr <jgullstr@abo.fi>
2012-01-31 00:46:08 +0200

    Add proposition stubs, PropositionView actions

--- OpenDemocracy/src/main/java/fi/opendemocracy/web/ui/PropositionView.java
+++ OpenDemocracy/src/main/java/fi/opendemocracy/web/ui/PropositionView.java
@@ -1,5 +1,6 @@
 package fi.opendemocracy.web.ui;
 
+import com.vaadin.event.Action;
 import com.vaadin.spring.roo.addon.annotations.RooVaadinEntityView;
 import com.vaadin.ui.Table;
 
@@ -10,12 +11,31 @@ import fi.opendemocracy.web.ThemeConstants;
 @RooVaadinEntityView(formBackingObject = fi.opendemocracy.domain.Proposition.class)
 public class PropositionView extends
 		AbstractEntityView<fi.opendemocracy.domain.Proposition> {
-
-	public PropositionView(){
+    // right-click actions
+    static final Action ACTION_NEW_PROPOSITION = new Action("Create new");
+    static final Action ACTION_OPEN_PROPOSITION = new Action("Open");
+    static final Action[] ACTIONS_MENU = new Action[] { ACTION_NEW_PROPOSITION, ACTION_OPEN_PROPOSITION };
+	
+    public PropositionView(){
 		setCaption(ThemeConstants.TAB_CAPTION_PROPOSITION);
 		setIcon(ThemeConstants.TAB_ICON_PROPOSITION);
-	}
-	
+        // add right-click menu
+        getTable().addActionHandler(new Action.Handler() {
+	        public Action[] getActions(Object target, Object sender) {
+	            return ACTIONS_MENU;
+	        }
+
+	        public void handleAction(Action action, Object sender, Object target) {
+	            if (ACTION_OPEN_PROPOSITION == action) {
+	            	getWindow().showNotification("TODO: Open");
+	            } else if (ACTION_NEW_PROPOSITION == action){
+	            	navigateTo("new");
+	            	getWindow().showNotification("TODO: Create");
+	            }
+	        }
+        });
+    }
+    
 	@Override
 	protected EntityEditor createForm() {
 		return new PropositionForm();
@@ -28,5 +48,6 @@ public class PropositionView extends
 
 		setupGeneratedColumns(table);
 	}
+	
 
 }
